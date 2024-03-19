29cd84d87e26747d7b971bc034369f75fd155d11
Andrey Mendonca <andrey.mmendonca@gmail.com>
2024-03-19 16:17:33 -0300

    SpringDataJPA - Inserção e lista de clientes

--- SpringBootDataJPA/src/main/java/com/example/DataJPA/SpringBootDataJpaApplication.java
+++ SpringBootDataJPA/src/main/java/com/example/DataJPA/SpringBootDataJpaApplication.java
@@ -1,10 +1,34 @@
 package com.example.DataJPA;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.DataJPA.domain.entity.Cliente;
+import com.example.DataJPA.domain.repository.Clientes;
 
 @SpringBootApplication
 public class SpringBootDataJpaApplication {
+	
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes) {
+		return args -> {
+			Cliente cliente = new Cliente();
+			cliente.setNome("Andrey");
+			clientes.salvar(cliente);
+			
+			Cliente cliente2 = new Cliente();
+			cliente2.setNome("Jorge");
+			clientes.salvar(cliente2);
+			
+			List<Cliente> todosClientes =  clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
