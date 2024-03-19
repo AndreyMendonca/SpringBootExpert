4652b9b987b17fc931dffdad07805446bc9e43fe
Andrey Mendonca <andrey.mmendonca@gmail.com>
2024-03-19 12:28:24 -0300

    Primeiras aulas

---
+++ SpringBootCoreFramework/src/main/java/com/example/demo/entities/Client.java
@@ -0,0 +1,19 @@
+package com.example.demo.entities;
+
+public class Client {
+	private String name;
+
+	public Client(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ SpringBootCoreFramework/src/main/java/com/example/demo/repository/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientRepository {
+	public void save() {
+		//salva o cliente
+	}
+}
