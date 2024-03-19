977786d3ef2377416128daf30612597bc23498f2
Andrey Mendonca <andrey.mmendonca@gmail.com>
2024-03-19 13:33:47 -0300

    Criando annotations

--- SpringBootCoreFramework/src/main/java/com/example/demo/SpringBootCoreFrameworkApplication.java
+++ SpringBootCoreFramework/src/main/java/com/example/demo/SpringBootCoreFrameworkApplication.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,16 @@ public class SpringBootCoreFrameworkApplication {
 	@GetMapping("/hello")
 	public String helloWorld() {
 		return applicationName;
+	}
+	
+	@cachorro
+	private Animal animal;
+	
+	@Bean(name="executarAnimal")
+	public CommandLineRunner executar() {
+		return args -> {
+			this.animal.fazerBarulho();
+		};
 	}
 	
 	public static void main(String[] args) {
