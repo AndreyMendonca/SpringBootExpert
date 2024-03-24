a663553664d10c8590fafeec699fb5c881c62f0f
Andrey Mendonca <andrey.mmendonca@gmail.com>
2024-03-23 22:12:58 -0300

    Conexão usando o JDBC

--- SpringBootDataJPA/src/main/java/com/example/DataJPA/SpringBootDataJpaApplication.java
+++ SpringBootDataJPA/src/main/java/com/example/DataJPA/SpringBootDataJpaApplication.java
@@ -22,11 +22,36 @@ public class SpringBootDataJpaApplication {
 			clientes.salvar(cliente);
 			
 			Cliente cliente2 = new Cliente();
+			
+			//salvar
 			cliente2.setNome("Jorge");
 			clientes.salvar(cliente2);
 			
 			List<Cliente> todosClientes =  clientes.obterTodos();
 			todosClientes.forEach(System.out::println);
+			
+			//update
+			todosClientes.forEach(c->{
+				c.setNome(c.getNome()+ " atualizado");
+				clientes.atualizar(c);
+			});
+			todosClientes =  clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+			
+			//buscar cliente
+			System.out.print("Busca por cliente com 'and' = ");
+			clientes.buscarPorNome("And").forEach(System.out::println);
+			
+			//deletando cliente
+			clientes.obterTodos().forEach(c->{
+				clientes.deletar(c.getId());
+			});		
+			todosClientes =  clientes.obterTodos();
+			if(todosClientes.isEmpty()) {
+				System.out.println("Todos os clientes deletesdos");
+			}else {
+				System.out.println("Fala ao deletar clientes");
+			}
 		};
 	}
 
