a737f702047dd78e818f588782beeb3a7facaa0a
Andrey Mendonca <andrey.mmendonca@gmail.com>
2024-06-04 19:10:17 -0300

    QueryMethods para String e valores numericos

--- ProjetoLocalizacao/src/main/java/com/example/localizacao/domain/repository/CidadeRepository.java
+++ ProjetoLocalizacao/src/main/java/com/example/localizacao/domain/repository/CidadeRepository.java
@@ -1,9 +1,28 @@
 package com.example.localizacao.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.localizacao.domain.entity.Cidade;
 
 public interface CidadeRepository extends JpaRepository<Cidade, Long>{
-
+	
+	//strings
+	List<Cidade> findByNome(String nome);
+	
+	@Query("select c from Cidade c where upper(c.nome) like upper(?1)")
+	List<Cidade> findByNomeLike(String nome);
+	
+	List<Cidade> findByNomeStartingWith(String nome);
+	
+	List<Cidade> findByNomeEndingWith(String nome);
+	
+	List<Cidade> findByNomeContaining(String nome);
+	
+	//numeros
+	List<Cidade> findByHabitantesLessThan(Long habitantes);
+	
+	List<Cidade> findByHabitantesGreaterThan(Long habitantes);
 }
